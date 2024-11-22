b0c14dc28ed7f774327709e69ec9c056695eee7a
Olmosbek510 <olmosbekorazboyev@gmail.com>
2024-11-23 01:34:49 +0500

    first commit

---
+++ src/main/java/com/inha/os/econtentbackend/EContentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.inha.os.econtentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EContentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EContentBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inha/os/econtentbackend/EContentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inha.os.econtentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EContentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
